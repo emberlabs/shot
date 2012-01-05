7430d360190d993446b7b391b55092dacb1bf507
Damian Bushong <damian@emberlabs.org>
2012-01-05 13:38:59 -0600

    working on controller interface

---
+++ Controller/ControllerInterface.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Shot Library
+ *-------------------------------------------------------------------
+ * @package     shot
+ * @author      emberlabs.org
+ * @copyright   (c) 2012 emberlabs.org
+ * @license     MIT License
+ * @link        https://github.com/emberlabs/shot
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace emberlabs\shot\Controller;
+namespace \OpenFlame\Framework\Route\RouteInstance;
+
+/**
+ * Shot - Controller interface
+ * 	     Provides controller prototype for controllers to fulfill.
+ *
+ * @package     shot
+ * @author      emberlabs.org
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/emberlabs/shot/
+ */
+interface ControllerInterface
+{
+	public function getName();
+
+	public function setName($name);
+
+	public function getTemplate();
+
+	public function setTemplate($template);
+
+	public function getRoute();
+
+	public function setRoute(RouteInstance $route);
+
+	public function runController();
+}
